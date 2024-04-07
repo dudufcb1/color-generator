2e37c850fd86d3000423808e08377b2b5183485b
Eduardo Gonzalez <heyimedd@gmail.com>
2024-04-07 05:31:34 -0600

    first commit

---
+++ src/ColorList.jsx
@@ -0,0 +1,15 @@
+import { nanoid } from 'nanoid';
+import React from 'react';
+import SingleColor from './SingleColor';
+
+const ColorList = ({ colors }) => {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />;
+      })}
+    </section>
+  );
+};
+
+export default ColorList;
---
+++ src/SingleColor.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const SingleColor = ({ color, index }) => {
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        navigator.clipboard.writeText('#' + color.hex);
+        toast.success('Copiado al portapapeles');
+      } catch (error) {}
+    } else {
+      toast.error('Clipboard Acess Not Available');
+    }
+  };
+  return (
+    <article
+      onClick={saveToClipboard}
+      className={index < 10 ? 'color' : 'color color-light'}
+      style={{ background: `#${color.hex}` }}
+    >
+      <p className="percent-value">{color.weight}%</p>
+      <p className="percent-value">#{color.hex}</p>
+    </article>
+  );
+};
+
+export default SingleColor;
